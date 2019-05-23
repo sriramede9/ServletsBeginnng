6ef82de4c29d89748306e5d948fc0596b8512f6c
sriramede9 <only4programming03@gmail.com>
2019-05-23 14:43:17 -0400

    current class no arg construc->init->service[req,resp][http-javaobjects]->destroy

--- src/com/one/FirstServlet.java
+++ src/com/one/FirstServlet.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,23 +28,62 @@ public class FirstServlet extends HttpServlet {
 //		PrintWriter pWriter = resp.getWriter();
 //		
 //		pWriter.println("<html><body style='background-color:cyan'><h1 align='center'>Hello \t"+fullname+" </h1></body></html>");
-		
-		//now to redirect
-		//resp.sendRedirect("https://www.google.com/");
-		
-		//now to local html page
-		
-		//resp.sendRedirect("test.html");
-		
-		//now to jsp
+
+		// now to redirect
+		// resp.sendRedirect("https://www.google.com/");
+
+		// now to local html page
+
+		// resp.sendRedirect("test.html");
+
+		// now to jsp
 		req.setAttribute("exp", fullname);
-		
-	RequestDispatcher requestDispatcher=	req.getRequestDispatcher("/output.jsp");
-	
-	requestDispatcher.forward(req, resp);
-		
+
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/output.jsp");
+
+		requestDispatcher.forward(req, resp);
+
 	}
 
 	// dopost
+
+	// life cycle explore
+
+	// current class obj first=1
+	public FirstServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+
+		System.out.println("Current class obj-1");
+	}
+
+	// init method-2
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+
+		System.out.println("I am init()-2");
+	}
+
+	// service method
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(req, res);
+
+		System.out.println("In service method() -3");
+	}
+
+	// now service method will send to do get,do post based on request.
+
+	// destroy method-4
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+
+		System.out.println("I am destroyed when the object in the service method is destroyed");
+	}
 
 }
